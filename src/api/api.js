582a6808fe8e3f71e2bd91ed88f91270a40c3586
sohee <topa11uu@gmail.com>
2024-07-19 23:41:08 +0900

    댓글 상세보기 기능 추가

--- src/api/api.js
+++ src/api/api.js
@@ -31,3 +31,19 @@ export async function getProductDetail(productId) {
     throw error;
   }
 }
+
+export async function getProductComment(productId) {
+  try {
+    const response = await fetch(
+      `https://panda-market-api.vercel.app/products/${productId}/comments?limit=10`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    console.error("Failed to fetch products detail:", error);
+    throw error;
+  }
+}
--- src/pages/ItemPage/ItemPage.jsx
+++ src/pages/ItemPage/ItemPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getProductDetail } from "../../api/api";
 import { useParams } from "react-router-dom";
 import { ReactComponent as HeartIcon } from "../../imgs/icon/icon_heart.svg";
+import Comment from "../../components/Comment";
 
 function ItemPage() {
   const [product, setProduct] = useState({});
@@ -47,40 +48,43 @@ function ItemPage() {
   const tags = Array.isArray(product.tags) ? product.tags : [];
 
   return (
-    <div className="flex p-6 m-4">
-      <img
-        src={product.images}
-        alt="상품 이미지"
-        className="w-[486px] h-[486px] object-cover rounded-2xl mb-4 mr-6"
-      />
-      <div>
-        <h2 className="text-2xl font-bold text-gray-800 mb-2">
-          {product.name}
-        </h2>
-        <p className="text-[40px] font-[600] mb-2">{product.price}원</p>
-        <p className="text-gray-700 mb-4">{product.description}</p>
-        {tags.length > 0 ? (
-          <div className="flex flex-wrap gap-2 mb-4">
-            {tags.map((tag, index) => (
-              <span
-                key={index}
-                className="bg-gray-200 text-gray-800 py-1 px-3 rounded-full text-sm"
-              >
-                {tag}
-              </span>
-            ))}
+    <div>
+      <div className="flex p-6 m-4">
+        <img
+          src={product.images}
+          alt="상품 이미지"
+          className="w-[486px] h-[486px] object-cover rounded-2xl mb-4 mr-6"
+        />
+        <div>
+          <h2 className="text-2xl font-bold text-gray-800 mb-2">
+            {product.name}
+          </h2>
+          <p className="text-[40px] font-[600] mb-2">{product.price}원</p>
+          <p className="text-gray-700 mb-4">{product.description}</p>
+          {tags.length > 0 ? (
+            <div className="flex flex-wrap gap-2 mb-4">
+              {tags.map((tag, index) => (
+                <span
+                  key={index}
+                  className="bg-gray-200 text-gray-800 py-1 px-3 rounded-full text-sm"
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+          ) : null}
+          <div className="flex items-center text-gray-500 text-sm">
+            <time className="mr-2">
+              {new Date(product.updatedAt).toLocaleDateString()}
+            </time>
+            <button className="flex justify-center items-center gap-1 w-[63px] h-[32px] rounded-[35px] border py-1 px-3">
+              <HeartIcon />
+              <span>{product.favoriteCount}</span>
+            </button>
           </div>
-        ) : null}
-        <div className="flex items-center text-gray-500 text-sm">
-          <time className="mr-2">
-            {new Date(product.updatedAt).toLocaleDateString()}
-          </time>
-          <button className="flex justify-center items-center gap-1 w-[63px] h-[32px] rounded-[35px] border py-1 px-3">
-            <HeartIcon />
-            <span>{product.favoriteCount}</span>
-          </button>
         </div>
       </div>
+      <Comment productId={product.id} />
     </div>
   );
 }
